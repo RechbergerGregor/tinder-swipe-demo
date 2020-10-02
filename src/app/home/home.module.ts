604cc26ab43794067dbfd356b0edf2ef7df634cf
Gregor Rechberger <g.rechberger@HTBLALeonding.onmicrosoft.com>
2020-10-02 09:43:37 +0200

    finish tutorial

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { TinderUiComponent } from '../tinder-ui/tinder-ui.component'
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,TinderUiComponent]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  cards;
+
+  constructor() {
+
+    this.cards = [];
+
+  }
+
+  loadTinderCards() {
+    this.cards = [
+      {
+        img: "https://placeimg.com/300/300/people",
+        title: "Demo card 1",
+        description: "This is a demo for Tinder like swipe cards"
+      },
+      {
+        img: "https://placeimg.com/300/300/animals",
+        title: "Demo card 2",
+        description: "This is a demo for Tinder like swipe cards"
+      },
+      {
+        img: "https://placeimg.com/300/300/nature",
+        title: "Demo card 3",
+        description: "This is a demo for Tinder like swipe cards"
+      },
+      {
+        img: "https://placeimg.com/300/300/tech",
+        title: "Demo card 4",
+        description: "This is a demo for Tinder like swipe cards"
+      },
+      {
+        img: "https://placeimg.com/300/300/arch",
+        title: "Demo card 5",
+        description: "This is a demo for Tinder like swipe cards"
+      }
+    ]
+  };
 
 }
